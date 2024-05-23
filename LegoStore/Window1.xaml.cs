dad6cb48d7864c54e970730324d18a330d3702a8
gopotatm@gmail.com <gopotatm@gmail.com>
2024-05-23 12:07:07 +0300

    Сделал корзину и немного покапался с фильтрацией

--- LegoStore/Window1.xaml.cs
+++ LegoStore/Window1.xaml.cs
@@ -26,5 +26,41 @@ namespace LegoStore
             ListView1.ItemsSource = items.entity.Products.ToList();
             ///hkhigk
         }
+        private void card_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void card_Click_1(object sender, RoutedEventArgs e)
+        {
+            card card = new card();
+            card.Show();
+            this.Close();
+        }
+
+
+        // 1
+        Products[] FindMain()
+        {
+            List<Products> mains = AppConnect.model0db.Products.ToList();
+
+            if (String.IsNullOrEmpty(findItems.Text) || String.IsNullOrWhiteSpace(findItems.Text))
+            {
+
+            }
+            else
+            {
+                mains = mains.Where(x => x.Name.ToLower().Contains(findItems.Text.ToLower())).ToList();
+            }
+
+            var mainAll = mains;
+            return mains.ToArray();
+        }
+
+        private void findItems_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ListView1.ItemsSource = FindMain();
+            //ssswssq
+        }
     }
 }
